43c5b2e0740707ecfe3f378f3d8013d9f961bea7
Nick Roberts <nickrhys@hotmail.com>
2014-12-18 16:39:26 +0000

    WIP adding event listeners

--- public/js/views/app_view.js
+++ public/js/views/app_view.js
@@ -1,12 +1,21 @@
 window.AppView = Backbone.View.extend({
 
+
   initialize: function(){
     var source   = $("#search-field-template").html();
     this.template = Handlebars.compile(source);
     this.render();
   },
+  events: {
+    "click #search-field-input": "updateSearch"
+  },
+
   render: function() {
-  $("#search-field-container").html(this.template({label: "search term"}));
+    $("#search-field-container").html(this.template({label: "search term"}));
+  },
+
+  updateSearch: function() {
+    console.log("updated search")
   }
 });
 
